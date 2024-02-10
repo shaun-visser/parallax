ef52107fcd0be849e562760028a679c02fa1c508
Shaun Visser <wedgelocals@gmail.com>
2024-02-10 15:42:08 +0200

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,97 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+'use client';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import { useEffect } from 'react';
+import { useRef } from 'react';
+import Parallax from 'parallax-js';
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const container = useRef(null);
+
+  useEffect(() => {
+    const scene = document.getElementById('scene');
+    const parallaxInstance = new Parallax(scene, {
+      pointerEvents: true,
+    });
+  }, []);
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      id="scene"
+      className="h-screen w-screen relative overflow-hidden bg-blue-100"
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      {/* <div
+        style={{
+          background:
+            'radial-gradient(ellipse at center, rgba(0,0,0,0) 65%, rgba(0, 0, 0, 0.7))',
+        }}
+        className="absolute pointer-events-none z-50 w-full h-full top-0 left-0"
+      ></div> */}
+      <div className="absolute w-full h-full" data-depth="0.05">
+        <div
+          className="bg-cover absolute -left-[5%] -top-[5%] w-[110%] -bottom-[5%]"
+          style={{ backgroundImage: "url('/bg.png')" }}
+        ></div>
+      </div>
+      <div className="absolute w-full h-full" data-depth="0.1">
+        <div className="absolute z-10 -left-[2%] bottom-[5.7%] md:bottom-[7.4%]] xl:bottom-[8.4%] 2xl:bottom-[11.4%] w-[98.046875%]">
+          <img
+            src="/mountain.png"
+            className="w-full max-w-none pointer-events-none"
+            alt=""
+          />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div
+        className="absolute w-full h-full z-20 pointer-events-none"
+        data-depth="0.20"
+      >
+        <div className="absolute -left-[2%] bottom-0 xl:-bottom-[2%] 2xl:-bottom-[2%] w-[80.710938%]">
+          <img
+            src="/clouds.png"
+            className="w-full max-w-none pointer-events-none"
+            alt=""
+          />
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div
+        className="absolute w-full h-full z-50 pointer-events-none"
+        data-depth="0.30"
+      >
+        <div className="absolute z-10 -right-[3%] bottom-0 xl:-bottom-[3%] 2xl:-bottom-[3%] w-[70.546875%]">
+          <img src="/foreground.png" className="w-full max-w-none" alt="" />
+        </div>
       </div>
+      <div
+        className="absolute w-full h-full z-10 pointer-events-auto"
+        data-depth="0.10"
+      >
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center uppercase text-white">
+          <h1 className="font-bold text-8xl">Cape Town</h1>
+          <h2 className="font-light text-5xl">South Africa</h2>
+        </div>
+      </div>
+      {/*
+      <img
+        src="/clouds.png"
+        className="absolute max-w-none transition duration-300 ease-in-expo pointer-events-none top-[90.705%] left-[48.194%] w-[194.583%] parallax transform -translate-y-1/2 -translate-x-1/2 z-40"
+        alt=""
+        data-depth="0.4"
+      />
+      <img
+        src="/foreground.png"
+        className="absolute max-w-none transition duration-300 ease-in-expo pointer-events-none top-[56.159%] left-[85.33%] w-[138.333%] parallax transform -translate-y-1/2 -translate-x-1/2 z-40"
+        alt=""
+        data-depth="0.5"
+      />
+      <div
+        className="text absolute transition duration-300 ease-in-expo parallax top-[calc(50%+48px)] left-1/2 -translate-x-1/2 -translate-y-1/2 text-center uppercase text-white z-30"
+        data-depth="0.6"
+      >
+        <h1 className="font-bold text-8xl">Cape Town</h1>
+        <h2 className="font-light text-5xl">South Africa</h2>
+      </div> */}
     </main>
   );
 }
